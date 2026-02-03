34b7e178ddf3abe3e87fd3fa1b76a58d6cf2e63c
swx <swxpat@qq.com>
2026-02-04 01:22:10 +0800

    Add js files.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "foliate-spm",
+    platforms: [
+        .iOS(.v26)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -16,7 +19,17 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "foliate-spm"
+            name: "foliate-spm",
+            path: "Sources/",
+            exclude: [
+                "Resources/foliate-js/LICENSE",
+                "Resources/foliate-js/README.md",
+                "Resources/foliate-js/reader.html",
+                "Resources/foliate-js/rollup/"
+            ],
+            resources: [
+                .process("Resources/foliate-js/"),
+            ],
         ),
         .testTarget(
             name: "foliate-spmTests",
